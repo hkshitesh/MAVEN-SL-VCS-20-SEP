4baf397af6bd42932b7b86fbb4752c3ec90250fc
unknown <Administrator@HiteshSharma-LTP.DDN.UPES.AC.IN>
2025-10-04 12:01:27 +0530

    mod() added

--- src/main/java/sl/vcs/MyCalc.java
+++ src/main/java/sl/vcs/MyCalc.java
@@ -19,6 +19,10 @@ public class MyCalc {
 	{
 		return a/b;
 	}
+	public int mod(int a, int b)
+	{
+		return a%b;
+	}
 
 	public static void main(String[] args) {
 		MyCalc calc = new MyCalc();
@@ -26,5 +30,6 @@ public class MyCalc {
 		System.out.println("Diff is :"+calc.diff(20, 10));
 		System.out.println("Mul is :"+calc.mul(20, 10));
 		System.out.println("Div is :"+calc.div(20, 10));
+		System.out.println("Mod is :"+calc.mod(20, 10));
 	}
 }
